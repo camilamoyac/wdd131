7ec7cd0ccf33b7a0e3931e4db97a00c004e479c2
camilamoyac <cami.csmc@gmail.com>
2025-01-11 13:19:44 -0300

    full homepage commit

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = `${today.getFullYear()}`;
+
+const lastModified = document.querySelector("#lastmodified");
+lastModified.textContent = `Last modified: ${document.lastModified}`;
