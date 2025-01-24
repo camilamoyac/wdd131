3d58d3cca14009af3ab791353bcbde484a889ae2
camilamoyac <cami.csmc@gmail.com>
2025-01-24 08:12:13 -0300

    added functionality

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,11 +1,29 @@
 const input = document.querySelector("#favchap");
-const button = document.querySelector("button");
-const list = document.querySelector("#list");
+const button = document.querySelector("button");  //add chapter
+const list = document.querySelector("#list");  //list of scriptures
 
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
+button.addEventListener("click", function(){
+    if (input.value.trim() === ""){
+        alert("Please enter a value.");
+        input.focus();
+        return;
+    }
+    else{
+        const li = document.createElement("li");  //creating list items
+        const deleteButton = document.createElement("button");  //❌ button
 
-li.textContent = input.value;
-button.textContent = "❌";
-li.append(deleteButton);
-list.append(li);
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener("click", function(){
+            list.removeChild(li);
+            input.focus();
+        })
+
+        input.value = "";
+        input.focus();
+    }
+});
+
