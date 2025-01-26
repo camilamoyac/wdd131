1727d6d57bf86caa30a08ad2a8e8a8411a1d602f
camilamoyac <cami.csmc@gmail.com>
2025-01-26 00:28:17 -0300

    done

---
+++ scripts/place.js
@@ -0,0 +1,21 @@
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = `${today.getFullYear()}`;
+
+const lastModified = document.querySelector("#lastmodified");
+lastModified.textContent = `Last modified: ${document.lastModified}`;
+
+
+const windch = document.querySelector("#windch");
+const temperature = 29;
+const windSpeed = 25;
+
+const calculateWindChill = (t, v) => {
+    return 13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16);
+};
+
+if (temperature <= 10 && windSpeed > 4.8){
+    windch.textContent = calculateWindChill(temperature, windSpeed).toFixed(1);
+} else{
+    windch.textContent = " N/A"
+}
