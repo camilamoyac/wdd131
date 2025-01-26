1eb7d538a7ed19d3b3a42fcfd5e36d78c484be8c
camilamoyac <cami.csmc@gmail.com>
2025-01-26 00:35:25 -0300

    made function into one line

--- scripts/place.js
+++ scripts/place.js
@@ -10,9 +10,7 @@ const windch = document.querySelector("#windch");
 const temperature = 29;
 const windSpeed = 25;
 
-const calculateWindChill = (t, v) => {
-    return 13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16);
-};
+const calculateWindChill = (t, v) => {return 13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16)};
 
 if (temperature <= 10 && windSpeed > 4.8){
     windch.textContent = calculateWindChill(temperature, windSpeed).toFixed(1);
